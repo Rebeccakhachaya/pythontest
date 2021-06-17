19f6a9899afdc4427fe3c0d97321926ca7db947b
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-06-17 11:43:49 +0300

    work

---
+++ pythontest.py
@@ -0,0 +1,55 @@
+x=[100,110,120,130,140,150]
+print(x)
+t=x
+for i in t :
+    t=5
+    print(i*5)
+
+
+def divisible_by_three(n):
+    n=100
+    i=range(1,100)
+    for x in i:
+         if x %3==0:
+             print(i)
+
+def flattens():
+    x=[[1,2],[3,4],[5,6]]
+    print(x)
+
+def smallest():
+      x=[1,3,5,7,9]
+      x.sort()
+      print(x)
+      smallest()
+x=["a","b","a","e","d","b","c","e","f","g",]
+print(x)
+x.remove
+
+
+print(x)
+def greeting(student_list):
+    for item in student_list:
+        birth_year=2021-item["age"]
+        name=item["name"]
+        print(f"Hello {name} you were born in the year {birth_year}")
+
+greeting([{"age": 19, "name": "Eunice"}, 
+ {"age": 21, "name": "john"}, {"age": 18, "name": "mercy"}, {"age": 22, "name": "Asha"}])
+
+
+
+
+
+
+class Rectangle():
+    def __init__(self,width,length):
+        self.width=width
+        self.length=length
+    def area():
+        return 6*5
+    def perimeter():
+        return 8*6
+     
+
+
